55a9114a54ec994547f6369d71b05fc3f2df2f4b
Shahrzad Faghihi <shahrzad.faghihi@gmail.com>
2012-11-24 16:42:54 -0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module ShahrzadBlog
 
     config.sass.preferred_syntax = :sass
 
-    
+    #for heroku
+    config.assets.initialize_on_precompile=false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
