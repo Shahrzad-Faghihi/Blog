62f4012e8c64605bef3ab82aff558866a32b6a99
Shahrzad Faghihi <shahrzad.faghihi@gmail.com>
2012-11-24 16:36:09 -0800

    deploy with heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121001235705) do
-
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20120924173048) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
